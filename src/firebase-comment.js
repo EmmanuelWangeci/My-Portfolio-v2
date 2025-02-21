7079d3117f2f5606a9d5058aec3ef20862ea8dea
Emmanuel Wangeci <emmanuelangeci@gmail.com>
2025-02-21 15:03:09 +0300

    Deleted comments

--- src/firebase-comment.js
+++ src/firebase-comment.js
@@ -3,13 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { collection, addDoc } from "@firebase/firestore";
 
+// Your web app's Firebase configuration (Replace with your credentials)
 const firebaseConfig = {
-    apiKey: "AIzaSyDJPq9a0YPoQYkpQ-Uaw7aXQRXzzqOKzFA",
-    authDomain: "web-kelas-tes.firebaseapp.com",
-    projectId: "web-kelas-tes",
-    storageBucket: "web-kelas-tes.appspot.com",
-    messagingSenderId: "890817433268",
-    appId: "1:890817433268:web:11e5258f8864a6174c11e1"
+    apiKey: "AIzaSyDCEsBr0wmhw2GQ4GV_ILR08vTrxCr9x84",
+    authDomain: "emmanuelwangeci-portfolio.firebaseapp.com",
+    projectId: "emmanuelwangeci-portfolio",
+    storageBucket: "emmanuelwangeci-portfolio.appspot.com",
+    messagingSenderId: "534161654146",
+    appId: "1:534161654146:web:9d25be8dc43d231bc34f9b"
 };
 
 // Initialize with a unique name
